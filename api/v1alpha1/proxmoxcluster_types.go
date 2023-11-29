6688418aa1a011cb00808bc87225797fd487de3e
Felix Wischke (65278) <felix.wischke@ionos.com>
2023-11-29 21:02:41 +0100

    cluster-class-cilium: initial commit

--- api/v1alpha1/proxmoxcluster_types.go
+++ api/v1alpha1/proxmoxcluster_types.go
@@ -67,6 +67,22 @@ type ProxmoxClusterSpec struct {
 	// DNSServers contains information about nameservers used by machines network-config.
 	// +kubebuilder:validation:MinItems=1
 	DNSServers []string `json:"dnsServers"`
+
+	// SshAuthorizedKeys contains the authorized keys deployed to the PROXMOX VMs
+    // +optional
+    SshAuthorizedKeys []string `json:"sshAuthorizedKeys"`
+
+    // KubernetesVersion contains the version of kubernetes installed by kubeadm
+    // +optional
+    KubernetesVersion string `json:"kubernetesVersion"`
+
+    // TemplateID is the ID for the template used for cloning proxmox VMs
+    // +optional
+    TemplateID int `json:"templateID"`
+
+    // VirtualIPNetworkInterface is the interface used by your VM
+    // +optional
+    VirtualIPNetworkInterface string `json:"virtualIPNetworkInterface"`
 }
 
 // SchedulerHints allows to pass the scheduler instructions to (dis)allow over- or enforce underprovisioning of resources.
