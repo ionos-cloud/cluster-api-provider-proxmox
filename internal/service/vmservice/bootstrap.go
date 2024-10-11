3c08bfbd33fa4413c10c03026ed1c305d069364b
tuunit <jan@larwig.com>
2024-10-11 12:59:48 +0200

    add metadata handling for proxmox node name

--- internal/service/vmservice/bootstrap.go
+++ internal/service/vmservice/bootstrap.go
@@ -72,7 +72,12 @@ func reconcileBootstrapData(ctx context.Context, machineScope *scope.MachineScop
 	network := cloudinit.NewNetworkConfig(nicData)
 
 	// create metadata renderer
-	metadata := cloudinit.NewMetadata(biosUUID, machineScope.Name())
+	metadata := cloudinit.NewMetadata(cloudinit.BaseCloudInitData{
+		Hostname:    machineScope.Name(),
+		Zone:        "dummy", // TODO: Figure out how to identify the availability zone inside a datacenter / proxmox cluster and make this generic enough to be used by other users of the provider (outside of ionos)
+		InstanceID:  biosUUID,
+		ProxmoxNode: machineScope.LocateProxmoxNode(),
+	})
 
 	injector := getISOInjector(machineScope.VirtualMachine, bootstrapData, metadata, network)
 	if err = injector.Inject(ctx); err != nil {
