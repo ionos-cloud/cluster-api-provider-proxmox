e9ee822f4a622e6199b9d9f2818962d51df05a20
Vic Kerr <wiktor.kerr@ionos.com>
2026-03-06 15:47:10 +0000

    update copyright

--- test/e2e/common.go
+++ test/e2e/common.go
@@ -2,7 +2,7 @@
 // +build e2e
 
 /*
-Copyright 2024 IONOS Cloud.
+Copyright 2024-2025 IONOS Cloud.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
