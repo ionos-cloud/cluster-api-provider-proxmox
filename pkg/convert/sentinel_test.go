573ee85d738ba6572e9c9ebc233b69b68ce97c9d
Vic Kerr <wiktor.kerr@ionos.com>
2026-04-28 12:19:19 +0000

    fixup! tool: bump golangci-lint from 2.9.0 to 2.11.4

--- pkg/convert/sentinel_test.go
+++ pkg/convert/sentinel_test.go
@@ -484,7 +484,7 @@ func TestGenerateSentinel(t *testing.T) {
 
 	// bool sentinel
 	s = generateSentinel("bool", &intCounter, "")
-	if s != "true" { //nolint:goconst // explicit string test
+	if s != "true" {
 		t.Errorf("bool sentinel = %q, want true", s)
 	}
 
